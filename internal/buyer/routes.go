0d0a7a59eab91cc3dbbbaac2cd9c2a1f126b8246
Karamjeet Sony <karamjeetsony2457@gmail.com>
2026-06-17 18:26:20 +0530

    feat:added Readme.md

--- internal/buyer/routes.go
+++ internal/buyer/routes.go
@@ -10,7 +10,7 @@ func BuyerRoutes(r *gin.RouterGroup) {
 	r.Use(middleware.UserMiddleware(), middleware.BuyerMiddleware())
 	service := NewService(false, 0)
 	buyerHandler := NewHandler(service)
-	r.GET("/events")
+	r.GET("/events", );
 	r.GET("/event/:Id")
 	r.POST("/event/:eventId/purchase/:productId", buyerHandler.BuyerEventHandler)
 }
